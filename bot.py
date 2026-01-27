9b5d7c3d50cc066b13319de4be6568e18fa42aa1
Michael Vasilchenko <muxaeji@gmail.com>
2026-01-27 13:04:28 +0300

    версия

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 from serial_number import generate_serial_number, parse_serial_number, format_serial_number, parse_serial_number
 
 # версия бота
-VERSION = "0.0.2"
+VERSION = "0.0.3"
 
 # Загружаем переменные окружения
 load_dotenv()
